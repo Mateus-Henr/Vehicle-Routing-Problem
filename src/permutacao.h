b187a8d1861dd67f07240989562ba71846d19370
Mateus Henrique <mateush.study@gmail.com>
2022-01-27 11:54:00 -0300

    Update on permutation.h

--- src/permutacao.h
+++ src/permutacao.h
@@ -1,7 +1,3 @@
-void imprimeArray(int *array, int tamanho);
-
-void mudarPosicao(int *a, int *b);
-
 void permutacao(int *arrayParaPermutar, int *arrayPermutacoes, int qtdElementos);
 
 int fatorial(int numero);
